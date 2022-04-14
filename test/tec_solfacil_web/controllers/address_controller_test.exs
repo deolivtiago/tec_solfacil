0f797e02f8178eb1dbc1a6b9c6193246a9cc415e
Tiago de Oliveira <olvrti@gmail.com>
2022-04-14 01:31:51 -0300

    test: update address controller tests

--- test/tec_solfacil_web/controllers/address_controller_test.exs
+++ test/tec_solfacil_web/controllers/address_controller_test.exs
@@ -2,13 +2,38 @@ defmodule TecSolfacilWeb.AddressControllerTest do
   use TecSolfacilWeb.ConnCase
 
   import TecSolfacil.LocationFixtures
+  import TecSolfacilWeb.ViaCEPFixtures
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
+
+    mock_client()
+  end
+
+  describe "create address" do
+    test "renders address when zip is valid and the address does not exist", %{conn: conn} do
+      conn = get(conn, Routes.address_path(conn, :show, "valid_zip"))
+
+      assert %{
+               "zip" => zip,
+               "street" => street,
+               "complement" => complement,
+               "district" => district,
+               "city" => city,
+               "state" => state
+             } = json_response(conn, 201)["data"]
+
+      assert zip == "12345678"
+      assert street == "street name"
+      assert complement == "address complement"
+      assert district == "district name"
+      assert city == "city name"
+      assert state == "state code"
+    end
   end
 
   describe "show address" do
-    test "renders address when zip is valid", %{conn: conn} do
+    test "renders address when zip is valid and the address exists", %{conn: conn} do
       address = address_fixture()
 
       conn = get(conn, Routes.address_path(conn, :show, address.zip))
