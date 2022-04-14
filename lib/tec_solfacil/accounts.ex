035b9f18c9f687da2d7c2aae1163857d673ab6ca
Tiago de Oliveira <olvrti@gmail.com>
2022-04-14 02:32:33 -0300

    feat: add user facade context

---
+++ lib/tec_solfacil/accounts.ex
@@ -0,0 +1,30 @@
+defmodule TecSolfacil.Accounts do
+  import Ecto.Query, warn: false
+  alias TecSolfacil.Repo
+
+  alias TecSolfacil.Accounts.User
+
+  def list_users, do: Repo.all(User)
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+
+  def change_user(%User{} = user, attrs \\ %{}) do
+    User.changeset(user, attrs)
+  end
+end
