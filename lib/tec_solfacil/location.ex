dc21a74600d09cea91d66585c5edee6da9f38168
Tiago de Oliveira <olvrti@gmail.com>
2022-04-14 20:52:21 -0300

    feat: generate csv from addresses

--- lib/tec_solfacil/location.ex
+++ lib/tec_solfacil/location.ex
@@ -11,4 +11,20 @@ defmodule TecSolfacil.Location do
     |> Address.changeset(attrs)
     |> Repo.insert()
   end
+
+  def generate_csv do
+    path = "addresses.csv"
+
+    data =
+      Address
+      |> Repo.all()
+      |> Enum.reduce(
+        "zip,street,complement,district,city,state",
+        &"#{&2}\n#{&1.zip},#{&1.street},#{&1.complement},#{&1.district},#{&1.city},#{&1.state}"
+      )
+
+    File.write!(path, data)
+
+    {:ok, path}
+  end
 end
