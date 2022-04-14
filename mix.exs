820f2ddb5b321d77f941e1edffda20a9f2c05d15
Tiago de Oliveira <olvrti@gmail.com>
2022-04-14 20:47:14 -0300

    chore: include oban

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule TecSolfacil.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:oban, "~> 2.11"},
       {:guardian, "~> 2.0"},
       {:argon2_elixir, "~> 2.0"},
       {:tesla, "~> 1.0"},
