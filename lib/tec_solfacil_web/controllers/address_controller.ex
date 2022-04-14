c91ed2c8732f86de9c9414c8b61a48b54ea939b4
Tiago de Oliveira <olvrti@gmail.com>
2022-04-13 22:59:35 -0300

    feat: add address controller

---
+++ lib/tec_solfacil_web/controllers/address_controller.ex
@@ -0,0 +1,16 @@
+defmodule TecSolfacilWeb.AddressController do
+  use TecSolfacilWeb, :controller
+
+  alias TecSolfacil.Location
+  alias TecSolfacil.Location.Address
+
+  def show(conn, %{"zip" => zip}) do
+    case Location.get_address_by_zip(zip) do
+      %Address{} = address ->
+        render(conn, "show.json", address: address)
+
+      nil ->
+        {:error, :not_found}
+    end
+  end
+end
