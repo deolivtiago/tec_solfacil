a7d65bf2de99945abc2bb6f8f76fab894ca4d5c2
Tiago de Oliveira <olvrti@gmail.com>
2022-04-14 01:31:11 -0300

    feat: update controller to create address from viacep

--- lib/tec_solfacil_web/controllers/address_controller.ex
+++ lib/tec_solfacil_web/controllers/address_controller.ex
@@ -3,16 +3,25 @@ defmodule TecSolfacilWeb.AddressController do
 
   alias TecSolfacil.Location
   alias TecSolfacil.Location.Address
+  alias TecSolfacilWeb.Http.ViaCEP
 
   action_fallback TecSolfacilWeb.FallbackController
 
-  def show(conn, %{"zip" => zip}) do
+  def create(conn, %{"zip" => zip}) do
+    with {:ok, %Address{} = address} <- ViaCEP.create_address_by_zip(zip) do
+      conn
+      |> put_status(:created)
+      |> render("show.json", address: address)
+    end
+  end
+
+  def show(conn, %{"zip" => zip} = params) do
     case Location.get_address_by_zip(zip) do
       %Address{} = address ->
         render(conn, "show.json", address: address)
 
       nil ->
-        {:error, :not_found}
+        create(conn, params)
     end
   end
 end
