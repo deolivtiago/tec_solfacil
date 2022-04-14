ce190cd1f9f45bf42810b0434f2e2820b6166178
Tiago de Oliveira <olvrti@gmail.com>
2022-04-14 02:31:46 -0300

    feat: add user schema

---
+++ lib/tec_solfacil/accounts/user.ex
@@ -0,0 +1,31 @@
+defmodule TecSolfacil.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string, virtual: true, redact: true
+    field :password_hash, :string, redact: true
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 4)
+    |> hash_password()
+  end
+
+  def changeset(attrs), do: changeset(%__MODULE__{}, attrs)
+
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp hash_password(changeset), do: changeset
+end
